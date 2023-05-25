ed32f71f00cbcbed2aa8fe63961f98965519a828
Milly-01 <workthabelowork@gmail.com>
2023-05-25 12:59:41 +0200

    Initial Commit

---
+++ frontend/peer_review/src/Components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+function App(){
+    return(
+        <div>
+            <h1>Hello Bafethu</h1>
+        </div>
+    );
+}
+
+export default App;
